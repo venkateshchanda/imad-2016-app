915e3e675f77715b1f16678f2c90aa1bb69ceeb2
venkateshchanda <chandavenkatesh524@gmail.com>
2016-09-29 04:05:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -128,7 +128,7 @@ app.get('/', function (req, res) {
 
 app.get("/:articleName",function(req,res){
     var articleName=req.params.articleName;
-    res.send(createTemplate(articleName));
+    res.send(createTemplate(articles[articleName]));
 });
 
 
